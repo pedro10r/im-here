21f62549689c6ed4199ff4197eafa4e3efe234d4
Pedro Santos <pedrosantos@Pedros-MacBook-Pro.local>
2022-10-17 15:51:59 -0300

    add function add participant

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Text, View, TextInput, TouchableOpacity, FlatList, Alert } from 'react-native';
 
 import { Participant } from '../../components/Participant';
@@ -5,27 +6,16 @@ import { Participant } from '../../components/Participant';
 import { styles } from './styles';
 
 export function Home() {
-  const participants = [
-    'Pedro',
-    'Adriara',
-    'João',
-    'Patricia',
-    'Maik',
-    'Jordan',
-    'LeBron',
-    'Durant',
-    'Westbrook',
-    'Curry',
-    'Green',
-    'Jota',
-    'Quest',
-    'Marcio',
-  ];
+  const [ participants, setParticipants ] = useState<string[]>([]);
+  const [ participantName, setParticipantName ] = useState('');
 
   function handleParticipantAdd() {
-    if (participants.includes('Pedro')) {
+    if (participants.includes(participantName)) {
       return Alert.alert('Participante existe', 'Já existe um participante na lista com esse nome');
     }
+
+    setParticipants(prevState => [...prevState, participantName]);
+    setParticipantName('');
   }
 
   function handleParticipantRemove(name: string) {
@@ -55,6 +45,8 @@ export function Home() {
           style={styles.input}
           placeholder='Nome do participante'
           placeholderTextColor='#6b6b6b'
+          onChangeText={setParticipantName}
+          value={participantName}
         />
 
         <TouchableOpacity
